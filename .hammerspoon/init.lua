c505d9778853d6f5f910152bdb299c3d448582a6
Lennard Wolf <wolf@amiconsult.de>
2025-01-23 11:33:07 +0100

    feat: hammerspoon config with first spoon for shiftit-like window sizing

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,19 @@
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+    left = { { 'ctrl', 'alt' }, 'left' },
+    right = { { 'ctrl', 'alt' }, 'right' },
+    up = { { 'ctrl', 'alt' }, 'up' },
+    down = { { 'ctrl', 'alt' }, 'down' },
+    upleft = { { 'ctrl', 'alt' }, '1' },
+    upright = { { 'ctrl', 'alt' }, '2' },
+    botleft = { { 'ctrl', 'alt' }, '3' },
+    botright = { { 'ctrl', 'alt' }, '4' },
+    maximum = { { 'ctrl', 'alt' }, 'return' },
+    toggleFullScreen = { { 'ctrl', 'alt' }, 'f' },
+    toggleZoom = { { 'ctrl', 'alt' }, 'z' },
+    center = { { 'ctrl', 'alt' }, 'm' },
+    nextScreen = { { 'ctrl', 'alt' }, 'n' },
+    previousScreen = { { 'ctrl', 'alt' }, 'p' },
+    resizeOut = { { 'ctrl', 'alt' }, '-' },
+    resizeIn = { { 'ctrl', 'alt' }, '.' }
+  })
